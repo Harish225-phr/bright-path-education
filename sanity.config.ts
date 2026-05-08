e8196a5a27758fa9b31f56f99867a29f10fb002e
Himtech <tech.himtech@gmail.com>
2026-05-08 13:00:39 +0530

    fix: update Sanity configuration to use hardcoded project ID and dataset

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { schemaTypes } from "./sanity/schemaTypes";
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID ?? "";
-const dataset = process.env.SANITY_STUDIO_DATASET ?? "production";
-
+// const projectId = process.env.SANITY_STUDIO_PROJECT_ID ?? "";
+// const dataset = process.env.SANITY_STUDIO_DATASET ?? "production";
+const projectId = "v9lzjatz";
+const dataset = "production";
 export default defineConfig({
   name: "default",
   title: "Bright Path Education CMS",
